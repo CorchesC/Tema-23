2bd4ac60f3338d4533b015d9f38cf03155bbe6c4
Corches Ciprian <cipriancorches@gmail.com>
2023-05-29 21:18:03 +0300

    probleme

--- Tema-23/functii.h
+++ Tema-23/functii.h
@@ -35,6 +35,26 @@ void afisareVector(int v[], int dim) {
 	cout << endl;
 
 }
+void citireMatrici(int x[100][100], int& m, int& n) {
+	ifstream f("numere.txt");
+	f >> m;
+	f >> n;
+	for (int i = 0; i < m; i++) {
+
+		for (int j = 0; j < n; j++) {
+
+			f >> x[i][j];
+		}
+	}
+}
+void afisareMatrici(int x[100][100], int& m, int& n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << x[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 int nrValoriStrictMaiMici(int a[], int d1, int b[], int d2) {
 	int nr = 0;
 	for (int i = 0; i < d1; i++) {
@@ -231,4 +251,259 @@ int produsMax(int v[], int d) {
 	}
 	int prod = max * max2;
 	return prod;
+}
+void generareMatrice1(int x[100][100], int m, int n) {
+	for (int i = 0; i <= 4; i++) {
+		for (int j = 0; j <= 4; j++) {
+			if (i < j && i + j < n - 1) {
+				x[i][j] = 1;
+			}
+			if (i < j && i + j > n - 1) {
+				x[i][j] = 2;
+			}
+			if (i > j && i + j > n - 1) {
+				x[i][j] = 3;
+			}
+			if (i > j && i + j < n - 1) {
+				x[i][j] = 4;
+			}
+			x[i][i] = 0;
+			x[i][(n - 1) - i] = 0;
+		}
+	}
+}
+void generareMatrice2(int x[100][100], int m, int n) {
+	for (int i = 0; i <= 4; i++) {
+		int nr = i + 1;
+		for (int j = 0; j <= 4; j++) {
+			if (j == 0) {
+				x[i][j] = i + 1;
+			}
+			else {
+				x[i][j] = nr + 5;
+				nr = x[i][j];
+			}
+		}
+	}
+}
+void generareMatrice3(int x[100][100], int m, int n) {
+	int nrMin = 1;
+	for (int i = 0; i <= 4; i++) {
+		for (int j = 0; j <= 4; j++) {
+			if (i % 2 == 0) {
+				x[i][j] = nrMin;
+				nrMin++;
+			}
+			if (i % 2 != 0) {
+				x[i][(n - 1) - j] = nrMin;
+				nrMin++;
+			}
+		}
+	}
+}
+void generareMatrice4(int x[100][100], int m, int n) {
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 5; j >= 0; j--) {
+			x[i][j] = m - ((j + 1) - (i + 1));
+		}
+	}
+}
+void generareMatrice5(int x[100][100], int m, int n) {
+	for (int i = 0; i <= 5; i++) {
+		int nr = 0;
+		for (int j = 0; j <= 2; j++) {
+			if (i == 0 || i == 5) {
+				x[i][j] = nr;
+				x[i][(n - 1) - j] = nr;
+				nr++;
+			}
+			if (i == 1 || i == 4) {
+				x[i][j] = nr + 1;
+				x[i][(n - 1) - j] = nr + 1;
+				nr++;
+			}
+			if (i == 2 || i == 3) {
+				x[i][j] = nr + 2;
+				x[i][(n - 1) - j] = nr + 2;
+				nr++;
+			}
+		}
+	}
+}
+void generareMatrice6(int x[100][100], int m, int n) {
+	for (int i = 1; i <= 6; i++) {
+		for (int j = 1; j <= 6; j++) {
+			x[i][j] = i % 3 + j % 3;
+			if (x[i][j] == 4) {
+				x[i][j] = 6;
+			}
+		}
+	}
+	for (int i = 1; i <= 6; i++) {
+		for (int j = 1; j <= 6; j++) {
+			cout << x[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void generareMatrice7(int x[100][100], int m, int n) {
+	for (int i = 5; i >= 1; i--) {
+		for (int j = 1; j < 6; j++) {
+			if (i == 5 || j == 1) {
+				x[i][j] = 5;
+			}
+			else {
+				x[i][j] = x[i][j - 1] % 2 + x[i + 1][j] % 2;
+			}
+		}
+	}
+	for (int i = 1; i < 6; i++) {
+		for (int j = 1; j < 6; j++) {
+			cout << x[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void afisareChar(int x[100][100], int& m, int& n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			char f = x[i][j];
+			cout << f << " ";
+		}
+		cout << endl;
+	}
+}
+void generareMatrice8(int x[100][100], int m, int n) {
+	for (int i = 0; i <= 6; i++) {
+		for (int j = 0; j <= 6; j++) {
+			if (j == 0 || j == 6) {
+				x[i][j] = '*';
+			}
+			else if (i <= 3 && (j + i == 6 || i == j)) {
+				x[i][j] = '*';
+			}
+			else {
+				x[i][j] = '-';
+			}
+		}
+	}
+}
+void generareMatrice9(int x[100][100], int m, int n) {
+	for (int i = 1; i <= 3; i++) {
+		for (int j = 1; j <= 3; j++) {
+			x[i][j] = i < j ? i : j;
+			x[7 - i][j] = x[i][7 - j] = x[7 - i][7 - j] = x[i][j];
+		}
+	}
+	for (int i = 1; i <= 6; i++) {
+		for (int j = 1; j <= 6; j++) {
+			cout << x[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void generareMatrice10(int x[100][100], int m, int n) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i == j) {
+				x[i][j] = 'A';
+			}
+			else if (i < j) {
+				x[i][j] = x[i][j - 1] + 1;
+			}
+			else if (j == 0) {
+				x[i][j] = x[i - 1][4];
+			}
+			else {
+				x[i][j] = x[i][j - 1] + 1;
+			}
+		}
+	}
+}
+void generareMatrice11(int x[100][100], int m, int n) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i == 0 || j == 0) {
+				x[i][j] = 2;
+			}
+			else {
+				x[i][j] = x[i - 1][j] + x[i][j - 1];
+			}
+		}
+	}
+}
+void generareMatrice12(int x[100][100], int m, int n) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (i % 2 == 0) {
+				if (j < 2) {
+					x[i][j] = 'a';
+				}
+				else {
+					x[i][j] = 'b';
+				}
+			}
+			if (i % 2 != 0) {
+				if (j < 2) {
+					x[i][j] = 'A';
+				}
+				else {
+					x[i][j] = 'B';
+				}
+			}
+		}
+	}
+}
+void generareMatrice13(int x[100][100], int m, int n) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (i == j) {
+				x[i][j] = 1;
+			}
+			else if (i + j == 3) {
+				x[i][j] = 7;
+			}
+			else {
+				x[i][j] = j + 1;
+			}
+		}
+	}
+}
+void generareMatrice14(int x[100][100], int m, int n) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i == j) {
+				x[i][j] = 'a';
+			}
+			else if (i < j) {
+				x[i][j] = x[i][j - 1] + 1;
+			}
+			else if (j == 0) {
+				x[i][j] = x[i - 1][j] + 1;
+			}
+			else {
+				x[i][j] = x[i][j - 1] - 1;
+			}
+		}
+	}
+}
+void generareMatrice15(int x[100][100], int m, int n) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i == 0) {
+				if (j < 4) {
+					x[i][j] = j + 1;
+				}
+				else {
+					x[i][j] = -2;
+				}
+			}
+			else if (j == 4) {
+				x[i][j] = -2;
+			}
+			else {
+				x[i][j] = x[i - 1][j] + x[i - 1][j + 1];
+			}
+		}
+	}
 }
--- Tema-23/solutii.h
+++ Tema-23/solutii.h
@@ -107,3 +107,79 @@ void sol11() {
 	int prod = produsMax(v, d);
 	cout << prod << endl;
 }
+void sol12() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice1(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol13() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice2(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol14() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice3(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol15() {
+	int x[100][100], m = 5, n = 6;
+	generareMatrice4(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol16() {
+	int x[100][100], m = 6, n = 6;
+	generareMatrice5(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol17() {
+	int x[100][100], m = 6, n = 6;
+	generareMatrice6(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol18() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice7(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol19() {
+	int x[100][100], m = 7, n = 7;
+	generareMatrice8(x, m, n);
+	afisareChar(x, m, n);
+}
+void sol20() {
+	int x[100][100], m = 6, n = 6;
+	generareMatrice9(x, m, n);
+	afisareMatrici(x, m, n);
+}
+void sol21() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice10(x, m, n);
+	afisareChar(x, m, n);
+}
+void sol22() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice11(x, m, n);
+	afisareMatrici(x, m, n); (x, m, n);
+}
+void sol23() {
+	int x[100][100], m = 4, n = 4;
+	generareMatrice12(x, m, n);
+	afisareChar(x, m, n);
+}
+void sol24() {
+	int x[100][100], m = 4, n = 4;
+	generareMatrice13(x, m, n);
+	afisareMatrici(x, m, n); (x, m, n);
+}
+void sol25() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice14(x, m, n);
+	afisareChar(x, m, n);
+}
+void sol26() {
+	int x[100][100], m = 5, n = 5;
+	generareMatrice15(x, m, n);
+	afisareMatrici(x, m, n); (x, m, n);
+
+}
